4241a72dd999f7dd126517c9a651dec62b35c9b3
Maciej Necka <macieknecka@gmail.com>
2023-10-06 21:28:52 +0200

    Travels loaded into DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,63 @@
-const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
-"2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
+'use strict';
+const uploadButton = document.querySelector('.uploader__input');
+uploadButton.addEventListener('change', travelsUpload);
 
-console.log( txt.split(/[\r\n]+/gm) );
+function travelsUpload(e) {
+  const file = e.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (renderEvent) {
+      const fileContent = renderEvent.target.result;
+      const lines = fileContent.split('\n');
+      let objArr = [];
+      for (let i = 0; i < lines.length; i++) {
+        const line = lines[i].trim();
+        if (line === '') continue;
+        const values = line.split(',');
+        const rowData = {
+          id: Number(values[0].replace(/"/g, '')),
+          title: values[1].replace(/"/g, ''),
+          description: values
+            .slice(2, -2)
+            .map((value) => value.replace(/"/g, '')),
+          adultPrice: Number(values[values.length - 2].match(/\d+/)[0]),
+          childPrice: Number(values[values.length - 1].match(/\d+/)[0]),
+        };
+        objArr.push(rowData);
+      }
+      const travelList = document.querySelector('.panel__excursions');
+      objArr.forEach(function (travel) {
+        travelList.innerHTML += `
+         <li class="excursions__item excursions__item--prototype">
+         <header class="excursions__header">
+         <h2 class="excursions__title">${travel.title}</h2>
+         <p class="excursions__description">${travel.description}</p>
+          </header>
+          <form class="excursions__form">
+            <div class="excursions__field">
+              <label class="excursions__field-name">
+                Dorosły: <span class="excursions__price">${travel.adultPrice}</span> PLN x
+                <input class="excursions__field-input" name="adults" />
+              </label>
+              </div>
+              <div class="excursions__field">
+              <label class="excursions__field-name">
+              Dziecko: <span class="excursions__price">${travel.childPrice}</span> PLN x
+              <input class="excursions__field-input" name="children" />
+              </label>
+              </div>
+              <div class="excursions__field excursions__field--submit">
+              <input
+              class="excursions__field-input excursions__field-input--submit"
+              value="dodaj do zamówienia"
+              type="submit"
+              />
+              </div>
+              </form>
+              </li>
+              `;
+      });
+    };
+    reader.readAsText(file);
+  }
+}
