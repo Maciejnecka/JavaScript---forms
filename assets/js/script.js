6893fa3d66339d4f3a2b708f5083d14fbb3ec81d
Maciej Necka <macieknecka@gmail.com>
2023-10-18 23:02:31 +0200

    Last handleSubmit left

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', init);
+const ulEl = document.querySelector('.order__errors-list');
+const formEl = document.querySelector('form');
 const uploadButton = document.querySelector('.uploader__input');
 uploadButton.addEventListener('change', travelsUpload);
 let totalOrderPrice = 0;
@@ -19,126 +22,10 @@ function travelsUpload(e) {
   const file = e.target.files[0];
   if (file) {
     const reader = new FileReader();
-    reader.onload = function (renderEvent) {
-      const fileContent = renderEvent.target.result;
-      const lines = fileContent.split('\n');
-      let objArr = [];
-      for (let i = 0; i < lines.length; i++) {
-        const line = lines[i].trim();
-        if (line === '') continue;
-        const values = line.split(',');
-        const rowData = {
-          title: values[1].replace(/"/g, ''),
-          description: values
-            .slice(2, -2)
-            .map((value) => value.replace(/"/g, '')),
-          adultPrice: Number(values[values.length - 2].match(/\d+/)[0]),
-          childPrice: Number(values[values.length - 1].match(/\d+/)[0]),
-        };
-        objArr.push(rowData);
-      }
-      const travelList = document.querySelector('.panel__excursions');
-      objArr.forEach(function (travel) {
-        travelList.innerHTML += `
-         <li class="excursions__item excursions__item--prototype">
-         <header class="excursions__header">
-         <h2 class="excursions__title">${travel.title}</h2>
-         <p class="excursions__description">${travel.description}</p>
-          </header>
-          <form class="excursions__form">
-            <div class="excursions__field">
-              <label class="excursions__field-name">
-                Dorosły: <span class="excursions__price--adult"> ${travel.adultPrice}</span> PLN 
-                <input class="excursions__field-input" name="adults" />
-              </label>
-              </div>
-              <div class="excursions__field">
-              <label class="excursions__field-name">
-              Dziecko: <span class="excursions__price--children"> ${travel.childPrice}</span> PLN 
-              <input class="excursions__field-input" name="children" />
-              </label>
-              </div>
-              <div class="excursions__field excursions__field--submit">
-              <input
-              class="excursions__field-input excursions__field-input--submit"
-              value="Dodaj do zamówienia"
-              type="submit"
-              />
-              </div>
-              </form>
-              </li>
-              `;
-      });
-      document
-        .querySelector('.panel__excursions')
-        .addEventListener('submit', function (e) {
-          e.preventDefault();
-          const form = e.target;
-          if (form.classList.contains('excursions__form')) {
-            const adultsInput = form.querySelector('input[name="adults"]');
-            const childrenInput = form.querySelector('input[name="children"]');
-            const adultPrice = form.querySelector(
-              'span.excursions__price--adult'
-            );
-            const childPrice = form.querySelector(
-              'span.excursions__price--children'
-            );
-            const travelEl = form
-              .closest('.excursions__item')
-              .querySelector('.excursions__title');
-            const travelTitle = travelEl.textContent;
-            const numAdults = Number(adultsInput.value);
-            const numChildren = Number(childrenInput.value);
-
-            const totalPrice =
-              numAdults * parseFloat(adultPrice.textContent) +
-              numChildren * parseFloat(childPrice.textContent);
-
-            const orderItem = {
-              title: travelTitle,
-              adultsAmmount: numAdults,
-              adultPrice: adultPrice.textContent,
-              childrenAmmount: numChildren,
-              childPrice: childPrice.textContent,
-              price: totalPrice,
-            };
-            const summaryItemID = `summary-item-${summaryItemCounter}`;
-            const summaryOrder = document.querySelector('.panel__summary');
-            if (orderItem.adultsAmmount > 0 || orderItem.childrenAmmount > 0) {
-              summaryOrder.innerHTML += ` 
-            <li class="summary__item summary__item--prototype" id="${summaryItemID}">
-            <h3 class="summary__title">
-            <span class="summary__name">${orderItem.title}</span>
-              <strong class="summary__total-price">${orderItem.price} PLN</strong>
-              <a href="" class="summary__btn-remove" title="usuń">X</a>
-              </h3>
-              <p class="summary__prices">dorośli: ${orderItem.adultsAmmount} x ${orderItem.adultPrice} PLN, dzieci: ${orderItem.childrenAmmount} x ${orderItem.childPrice} PLN</p>
-              </li>
-              `;
-              const totalOrderPriceElement = document.querySelector(
-                '.order__total-price'
-              );
-              totalOrderPrice += totalPrice;
-              totalOrderPriceElement.innerHTML = `Razem: <span class="order__total-price-value">${totalOrderPrice} PLN</span>
-          `;
-              adultsInput.value = '';
-              childrenInput.value = '';
-            } else {
-              alert('Proszę wybrać bilet!');
-              adultsInput.value = '';
-              childrenInput.value = '';
-            }
-            summaryItemCounter++;
-          }
-        });
-    };
+    reader.onload = processFileContents;
     reader.readAsText(file);
   }
 }
-
-document.addEventListener('DOMContentLoaded', init);
-const ulEl = document.querySelector('.order__errors-list');
-const formEl = document.querySelector('form');
 
 // Functions
 function init() {
@@ -226,4 +113,124 @@ function removeSummaryItem(itemID) {
     );
     totalOrderPriceElement.innerHTML = `Razem: <span class="order__total-price-value">${totalOrderPrice} PLN</span>`;
   }
+}
+
+function processFileContents(readerEvent) {
+  const fileContent = readerEvent.target.result;
+  const objArr = parseFileContent(fileContent);
+  objArr.forEach(addTravelItemToDOM);
+  document
+    .querySelector('.panel__excursions')
+    .addEventListener('submit', function (e) {
+      e.preventDefault();
+      handleExcursionFormSubmit(e);
+    });
+}
+
+function parseFileContent(fileContent) {
+  const lines = fileContent.split('\n');
+  let objArr = [];
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+    if (line === '') continue;
+    const values = line.split(',');
+    const rowData = {
+      title: values[1].replace(/"/g, ''),
+      description: values.slice(2, -2).map((value) => value.replace(/"/g, '')),
+      adultPrice: Number(values[values.length - 2].match(/\d+/)[0]),
+      childPrice: Number(values[values.length - 1].match(/\d+/)[0]),
+    };
+    objArr.push(rowData);
+  }
+  return objArr;
+}
+
+function handleExcursionFormSubmit(e) {
+  e.preventDefault();
+  const form = e.target;
+  if (form.classList.contains('excursions__form')) {
+    const adultsInput = form.querySelector('input[name="adults"]');
+    const childrenInput = form.querySelector('input[name="children"]');
+    const adultPrice = form.querySelector('span.excursions__price--adult');
+    const childPrice = form.querySelector('span.excursions__price--children');
+    const travelEl = form
+      .closest('.excursions__item')
+      .querySelector('.excursions__title');
+    const travelTitle = travelEl.textContent;
+    const numAdults = Number(adultsInput.value);
+    const numChildren = Number(childrenInput.value);
+
+    const totalPrice =
+      numAdults * parseFloat(adultPrice.textContent) +
+      numChildren * parseFloat(childPrice.textContent);
+
+    const orderItem = {
+      title: travelTitle,
+      adultsAmmount: numAdults,
+      adultPrice: adultPrice.textContent,
+      childrenAmmount: numChildren,
+      childPrice: childPrice.textContent,
+      price: totalPrice,
+    };
+    const summaryItemID = `summary-item-${summaryItemCounter}`;
+    const summaryOrder = document.querySelector('.panel__summary');
+    if (orderItem.adultsAmmount > 0 || orderItem.childrenAmmount > 0) {
+      summaryOrder.innerHTML += ` 
+            <li class="summary__item summary__item--prototype" id="${summaryItemID}">
+            <h3 class="summary__title">
+            <span class="summary__name">${orderItem.title}</span>
+              <strong class="summary__total-price">${orderItem.price} PLN</strong>
+              <a href="" class="summary__btn-remove" title="usuń">X</a>
+              </h3>
+              <p class="summary__prices">dorośli: ${orderItem.adultsAmmount} x ${orderItem.adultPrice} PLN, dzieci: ${orderItem.childrenAmmount} x ${orderItem.childPrice} PLN</p>
+              </li>
+              `;
+      const totalOrderPriceElement = document.querySelector(
+        '.order__total-price'
+      );
+      totalOrderPrice += totalPrice;
+      totalOrderPriceElement.innerHTML = `Razem: <span class="order__total-price-value">${totalOrderPrice} PLN</span>
+          `;
+      adultsInput.value = '';
+      childrenInput.value = '';
+    } else {
+      alert('Proszę wybrać bilet!');
+      adultsInput.value = '';
+      childrenInput.value = '';
+    }
+    summaryItemCounter++;
+  }
+}
+
+function addTravelItemToDOM(travel) {
+  const travelList = document.querySelector('.panel__excursions');
+  travelList.innerHTML += `
+         <li class="excursions__item excursions__item--prototype">
+         <header class="excursions__header">
+         <h2 class="excursions__title">${travel.title}</h2>
+         <p class="excursions__description">${travel.description}</p>
+          </header>
+          <form class="excursions__form">
+            <div class="excursions__field">
+              <label class="excursions__field-name">
+                Dorosły: <span class="excursions__price--adult"> ${travel.adultPrice}</span> PLN 
+                <input class="excursions__field-input" name="adults" />
+              </label>
+              </div>
+              <div class="excursions__field">
+              <label class="excursions__field-name">
+              Dziecko: <span class="excursions__price--children"> ${travel.childPrice}</span> PLN 
+              <input class="excursions__field-input" name="children" />
+              </label>
+              </div>
+              <div class="excursions__field excursions__field--submit">
+              <input
+              class="excursions__field-input excursions__field-input--submit"
+              value="Dodaj do zamówienia"
+              type="submit"
+              />
+              </div>
+              </form>
+              </li>
+              `;
 }
